87fa7e5a191d2022bba3a6c988f73e31700b5550
Sudeep Gautam <sudeepgautam777@gmail.com>
2025-11-04 19:42:35 +0545

    Implementing getAllContacts

--- backend/src/controllers/contactController.js
+++ backend/src/controllers/contactController.js
@@ -8,3 +8,16 @@ export const createContact = async (req, res) => {
     res.status(400).json({ status: 'fail', message: err.message });
   }
 };
+
+export const getAllContacts = async (req, res) => {
+  try {
+    const contacts = await Contact.find();
+    res.status(200).json({
+      status: 'success',
+      results: contacts.length,
+      data: { contacts },
+    });
+  } catch (err) {
+    res.status(500).json({ status: 'error', message: err.message });
+  }
+};
--- backend/src/routes/contactRoute.js
+++ backend/src/routes/contactRoute.js
@@ -1,8 +1,12 @@
 import express from 'express';
-import { createContact } from '../controllers/contactController.js';
+import {
+  createContact,
+  getAllContacts,
+} from '../controllers/contactController.js';
 
 const router = express.Router();
 
 router.post('/', createContact);
+router.get('/', getAllContacts);
 
 export default router;
