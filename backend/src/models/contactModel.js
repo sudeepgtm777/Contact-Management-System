468bdddcf1e2b306e49897618c7ffdfce92062fe
Sudeep Gautam <sudeepgautam777@gmail.com>
2025-11-04 19:38:58 +0545

    Use of Validation

--- backend/src/models/contactModel.js
+++ backend/src/models/contactModel.js
@@ -1,10 +1,19 @@
 import mongoose from 'mongoose';
+import validator from 'validator';
 
 const contactSchema = new mongoose.Schema(
   {
     user_name: {
       type: String,
       required: [true, 'Contact must have a name'],
+      validate: {
+        validator: function (val) {
+          // Must contain at least two words, each made of alphabetic letters only
+          return /^[A-Za-z]+(?:\s+[A-Za-z]+)+$/.test(val.trim());
+        },
+        message:
+          'Please enter your full name (first and last), using alphabets only.',
+      },
     },
     user_logo: {
       type: String,
@@ -15,6 +24,12 @@ const contactSchema = new mongoose.Schema(
       required: [true, 'Contact must have an email'],
       unique: true,
       lowercase: true,
+      validate: [
+        {
+          validator: validator.isEmail,
+          message: 'Use a valid Email!',
+        },
+      ],
     },
     user_phone: {
       type: String,
