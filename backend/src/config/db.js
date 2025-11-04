3f46da55a182fe9839dd24665c103a76702c5b66
Sudeep Gautam <sudeepgautam777@gmail.com>
2025-11-04 20:09:01 +0545

    Adding termination point with process.exit()

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,10 +2,13 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DATABASE);
-
-    console.log('DataBase Connected Succesfully!!!');
+    await mongoose.connect(process.env.DATABASE, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Database Connected Successfully!!!');
   } catch (err) {
-    console.log('Error connecting to database', err);
+    console.error('Error connecting to database:', err);
+    process.exit(1);
   }
 };
