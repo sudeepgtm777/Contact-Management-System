d3e9df3d78361c473c22f02cc128f7aece160779
Sudeep Gautam <sudeepgautam777@gmail.com>
2025-11-05 12:28:58 +0545

    Use of cookieParser, Implementing authRoutes and globalErrorHandler implementation

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,9 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 import { connectDB } from './config/db.js';
 import contactRoutes from './routes/contactRoute.js';
+import authRoutes from './routes/authRoute.js';
+import globalErrorHandler from './controllers/errorController.js';
 
 import dotenv from 'dotenv';
 import path from 'path';
@@ -10,11 +13,19 @@ dotenv.config({ path: path.resolve('src/config/config.env') });
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/contacts', contactRoutes);
+app.use('/api/auth', authRoutes);
 
 connectDB();
 const port = process.env.PORT;
+
+app.use((req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 app.listen(port, () => {
   console.log(`Server Started on Port ${port}`);
