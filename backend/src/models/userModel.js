c93aed887f6cdcc43566dbc7b7e166dc406db7d9
Sudeep Gautam <sudeepgautam777@gmail.com>
2025-11-05 12:18:33 +0545

    Removing crypto package

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -1,4 +1,7 @@
+import crypto from 'crypto';
 import mongoose from 'mongoose';
+import validator from 'validator';
+import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -58,6 +61,66 @@ const userSchema = new mongoose.Schema({
     select: false,
   },
 });
+
+userSchema.pre('save', async function (next) {
+  // Run this if password was modified!!
+  if (!this.isModified('password')) return next();
+
+  // Match the password with cost of 10
+  this.password = await bcrypt.hash(this.password, 10);
+
+  // Delete the password confirm field!!
+  this.passwordConfirm = undefined;
+  next();
+});
+
+userSchema.pre('save', function (next) {
+  if (!this.isModified('password') || this.isNew) return next();
+
+  this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
+
+userSchema.methods.correctPassword = async function (
+  candiatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candiatePassword, userPassword);
+};
+
+userSchema.pre(/^find/, function (next) {
+  // This points to the current query that is there. It simply runs before any query with .find()
+  this.find({ active: { $ne: false } });
+  next();
+});
+
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    const changedTimeStamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10
+    );
+    return JWTTimestamp < changedTimeStamp;
+  }
+
+  // False means no change in data
+  return false;
+};
+
+userSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString('hex');
+
+  this.passwordResetToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  console.log(resetToken, this.passwordResetToken);
+
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
+};
 
 const User = mongoose.model('User', userSchema);
 
