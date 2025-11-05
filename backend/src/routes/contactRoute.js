fe33d9a5120e188f64733fea732092c5a37a6659
Sudeep Gautam <sudeepgautam777@gmail.com>
2025-11-05 12:22:13 +0545

    Use of protect route

--- backend/src/routes/contactRoute.js
+++ backend/src/routes/contactRoute.js
@@ -6,8 +6,11 @@ import {
   getContact,
   updateContact,
 } from '../controllers/contactController.js';
+import { protect } from '../controllers/authController.js';
 
 const router = express.Router();
+
+router.use(protect);
 
 router.post('/', createContact);
 router.get('/', getAllContacts);
