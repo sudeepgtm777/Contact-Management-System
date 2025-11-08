011a0251fa8c144e06c539f8b75c47ffd3337452
Sudeep Gautam <sudeepgautam777@gmail.com>
2025-11-08 19:53:38 +0545

    Implementing corcs in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import { connectDB } from './config/db.js';
 import contactRoutes from './routes/contactRoute.js';
 import userRoutes from './routes/userRoute.js';
 import authRoutes from './routes/authRoute.js';
 import globalErrorHandler from './controllers/errorController.js';
+import AppError from './utils/appError.js';
 
 import dotenv from 'dotenv';
 import path from 'path';
@@ -13,12 +15,29 @@ dotenv.config({ path: path.resolve('src/config/config.env') });
 
 const app = express();
 
+if (process.env.NODE_ENV !== 'production') {
+  app.use(
+    cors({
+      origin: 'http://localhost:5173',
+      credentials: true,
+    })
+  );
+}
+
 app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/contacts', contactRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client', 'dist', 'index.html'));
+  });
+}
 
 connectDB();
 const port = process.env.PORT;
