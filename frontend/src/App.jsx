5d2a2e4f141cafa5217c7d9a98d269947834550d
Sudeep Gautam <sudeepgautam777@gmail.com>
2025-11-08 20:20:12 +0545

    Setting up routes

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Route, Routes } from 'react-router';
+import { HomePage } from './pages/HomePage';
+import MainLayout from './layouts/MainLayout';
+import NotFoundPage from './pages/NotFoundPage';
+import LoginPage from './pages/LoginPage';
+import SignUp from './pages/SignupPage';
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<MainLayout />}>
+          <Route path='*' element={<NotFoundPage />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/sign-in' element={<LoginPage />} />
+        </Route>
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
