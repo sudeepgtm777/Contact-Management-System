dc1cd6c92a3f60c88c288cd9bc41e569c3797509
Sudeep Gautam <sudeepgautam777@gmail.com>
2025-11-08 19:52:55 +0545

    Implementing isLoggedIn route to check user is active or not in client side

--- backend/src/routes/authRoute.js
+++ backend/src/routes/authRoute.js
@@ -1,10 +1,16 @@
 import express from 'express';
-import { login, logout, register } from '../controllers/authController.js';
+import {
+  isLoggedIn,
+  login,
+  logout,
+  register,
+} from '../controllers/authController.js';
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
 router.get('/logout', logout);
+router.get('/isLoggedIn', isLoggedIn);
 
 export default router;
