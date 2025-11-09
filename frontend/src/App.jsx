278c220a44d64dd75bb3b6a1e5cd73efd6df7884
Sudeep Gautam <sudeepgautam777@gmail.com>
2025-11-09 20:03:52 +0545

    Adding route for editContactPage.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Route, Routes } from 'react-router';
+import { Route, Routes } from 'react-router-dom';
 import { HomePage } from './pages/HomePage';
 import MainLayout from './layouts/MainLayout';
 import NotFoundPage from './pages/NotFoundPage';
 import LoginPage from './pages/LoginPage';
 import SignUp from './pages/SignupPage';
 import AddContactPage from './pages/AddContactPage';
+import EditContactPage from './pages/EditContactPage';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <Route path='/' element={<MainLayout />}>
           <Route index element={<HomePage />} />
           <Route path='add-contact' element={<AddContactPage />} />
+          <Route path='edit-contact/:id' element={<EditContactPage />} />
           <Route path='*' element={<NotFoundPage />} />
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/sign-in' element={<LoginPage />} />
