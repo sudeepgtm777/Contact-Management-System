a17f34e4313b9b5ae61f3cc82801a2a7f8d1efcd
Sudeep Gautam <sudeepgautam777@gmail.com>
2025-11-10 15:20:18 +0545

    Debugginf config file path

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-dotenv.config({ path: path.join(__dirname, '../../config/config.env') });
+dotenv.config({ path: path.join(__dirname, '/config/config.env') });
 
 const app = express();
 
