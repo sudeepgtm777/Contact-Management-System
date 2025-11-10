a0dafe91d6a20ff0b7c9bef4aa538e595b7ccf00
Sudeep Gautam <sudeepgautam777@gmail.com>
2025-11-10 13:18:13 +0545

    Debugging in app.js for deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -34,10 +34,10 @@ app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist')));
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
   app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client', 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
   });
 }
 
