5c7c71b26dc1f43e2e15220e41b117240cc551d8
Sudeep Gautam <sudeepgautam777@gmail.com>
2025-11-23 21:24:00 +0545

    Adding route for /public/img/users as /users

--- backend/src/app.js
+++ backend/src/app.js
@@ -31,6 +31,8 @@ if (process.env.NODE_ENV !== 'production') {
 app.use(express.json());
 app.use(cookieParser());
 
+app.use('/users', express.static(path.join(__dirname, '../public/img/users')));
+
 app.use('/api/contacts', contactRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
