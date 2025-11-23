e43197f0dfb4d6974d99b8c8a777754a6051d727
Sudeep Gautam <sudeepgautam777@gmail.com>
2025-11-23 21:28:28 +0545

    Adding route for updateMe,uploadUserPhoto,resizeUserPhoto

--- backend/src/routes/userRoute.js
+++ backend/src/routes/userRoute.js
@@ -4,11 +4,16 @@ import {
   getAllUsers,
   getUser,
   updateUser,
+  updateMe,
+  uploadUserPhoto,
+  resizeUserPhoto,
 } from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.use(protect);
+
+router.patch('/updateMe', uploadUserPhoto, resizeUserPhoto, updateMe);
 
 router.get('/', getAllUsers);
 router.get('/:id', getUser);
