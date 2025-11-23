80a3bc9ca0751ca042518040bd4d879462194b9e
Sudeep Gautam <sudeepgautam777@gmail.com>
2025-11-23 21:35:28 +0545

    Adding route for UserContext

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,14 +2,17 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
+import { UserProvider } from './context/UserContext.jsx';
 import './index.css';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
-      <Toaster />
+      <UserProvider>
+        <App />
+        <Toaster />
+      </UserProvider>
     </BrowserRouter>
   </StrictMode>
 );
