2837f2a46f706b80efc22b17d983982ca7ffe70a
Sudeep Gautam <sudeepgautam777@gmail.com>
2025-11-24 14:17:03 +0545

    Adding route for changing Password

--- backend/src/routes/authRoute.js
+++ backend/src/routes/authRoute.js
@@ -4,6 +4,8 @@ import {
   login,
   logout,
   register,
+  protect,
+  updateMyPassword,
 } from '../controllers/authController.js';
 
 const router = express.Router();
@@ -12,5 +14,6 @@ router.post('/register', register);
 router.post('/login', login);
 router.get('/logout', logout);
 router.get('/isLoggedIn', isLoggedIn);
+router.patch('/updateMyPassword', protect, updateMyPassword);
 
 export default router;
