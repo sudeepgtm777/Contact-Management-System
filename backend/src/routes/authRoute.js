dace90a5e94a9334aed9cf6da90636074207be55
Sudeep Gautam <sudeepgautam777@gmail.com>
2025-12-02 19:14:03 +0545

    Adding route for verify-email, forgot-password, reset-password

--- backend/src/routes/authRoute.js
+++ backend/src/routes/authRoute.js
@@ -1,19 +1,25 @@
 import express from 'express';
 import {
-  isLoggedIn,
+  register,
   login,
   logout,
-  register,
+  isLoggedIn,
   protect,
   updateMyPassword,
+  verifyEmail,
+  forgotPassword,
+  resetPassword,
 } from '../controllers/authController.js';
 
 const router = express.Router();
 
 router.post('/register', register);
+router.get('/verify-email', verifyEmail);
 router.post('/login', login);
 router.get('/logout', logout);
 router.get('/isLoggedIn', isLoggedIn);
+router.post('/forgot-password', forgotPassword);
+router.post('/reset-password', resetPassword);
 router.patch('/updateMyPassword', protect, updateMyPassword);
 
 export default router;
