a830716b8cb59ecbd5b81033b9e469891df82b8d
Sudeep Gautam <sudeepgautam777@gmail.com>
2025-12-02 19:08:46 +0545

    Adding config file at the start path

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,3 +1,12 @@
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.join(__dirname, './config/config.env') });
+
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
@@ -7,15 +16,6 @@ import userRoutes from './routes/userRoute.js';
 import authRoutes from './routes/authRoute.js';
 import globalErrorHandler from './controllers/errorController.js';
 import AppError from './utils/appError.js';
-
-import dotenv from 'dotenv';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-dotenv.config({ path: path.join(__dirname, '/config/config.env') });
 
 const app = express();
 
@@ -49,14 +49,13 @@ if (process.env.NODE_ENV === 'production') {
 
 connectDB();
 
-const port = process.env.PORT || 3000;
-
 app.use((req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
 app.use(globalErrorHandler);
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
